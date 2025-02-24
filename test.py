744cf3b414bbc6c8b4b7bb759be773186162f14f
Paul Peytevin <ppeytevin@gmail.com>
2025-02-24 23:06:36 +0100

    work on motor calibration

---
+++ test.py
@@ -0,0 +1,5 @@
+from motor_control import main
+
+main("FA0191")
+# If I understood correctly, the above function should trigger the search of the home position of the motor.
+# The motor should move to the home position and stop.
